4a2c3d0b9f41a9ccd80c5351875b122db59d2ece
tusk <tusk@mushka>
2014-05-06 17:43:16 -1000

    New build system with partial compilation

--- rpi-firmware/rpid/opqwebsocket.hpp
+++ rpi-firmware/rpid/opqwebsocket.hpp
@@ -13,7 +13,7 @@ public:
 private:
     std::string wsUrl_;
     easywsclient::WebSocket::pointer ws_;
-    void handleMessage(std::string & message);
+    void handleMessage(std::string &message);
 };
 
 #endif // OPQWEBSOCKET_HPP
