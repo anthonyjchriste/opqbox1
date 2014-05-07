220d2fe792fb5c5b9c4a3549e28aff7979ccacb5
anthonyjchriste <anthony.christe@gmail.com>
2014-05-06 16:49:40 -1000

    Added some ws code

---
+++ rpi-firmware/rpid/opqwebsocket.hpp
@@ -0,0 +1,19 @@
+#ifndef OPQWEBSOCKET_HPP
+#define OPQWEBSOCKET_HPP
+
+#import "contrib/easywsclient/easywsclient.hpp"
+
+#include<string>
+
+class OpqWebsocket
+{
+public:
+    OpqWebsocket();
+    void listen();
+private:
+    std::string wsUrl_;
+    easywsclient::WebSocket::pointer ws_;
+    void handleMessage(const std::string & message);
+};
+
+#endif // OPQWEBSOCKET_HPP
