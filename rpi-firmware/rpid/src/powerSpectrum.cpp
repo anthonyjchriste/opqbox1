c8810f4a6398c8641d23c64a2de82dc0460695c9
tusk <tusk@mushka>
2014-05-09 14:49:09 -1000

    changed fft implementation

--- rpi-firmware/rpid/src/powerSpectrum.cpp
+++ rpi-firmware/rpid/src/powerSpectrum.cpp
@@ -1,7 +1,9 @@
 #include "powerSpectrum.hpp"
 //#include <gsl/gsl_errno.h>
 #include <gsl/gsl_fft_real.h>
-#include <gsl/gsl_fft_halfcomplex.h>
+#include <gsl/gsl_fft_complex.h>
+//#include <gsl/gsl_fft_halfcomplex.h>
+
 #include <vector>
 
 vector<double> powerSpectrum(vector<double> input)
@@ -13,16 +15,15 @@ vector<double> powerSpectrum(vector<double> input)
         reals[i*2] = input[i];
         reals[i*2+1] = 0;
     }
-    gsl_fft_real_wavetable * real;
-    //gsl_fft_halfcomplex_wavetable * hc;
-    gsl_fft_real_workspace * work;
+    gsl_fft_complex_wavetable * complex;
+    gsl_fft_complex_workspace * work;
 
-    work = gsl_fft_real_workspace_alloc(length);
-    real = gsl_fft_real_wavetable_alloc(length);
-    gsl_fft_real_transform (reals, 1, length, real, work);
+    work = gsl_fft_complex_workspace_alloc(length);
+    complex = gsl_fft_complex_wavetable_alloc(length);
+    gsl_fft_complex_transform (reals, 1, length, complex, work, gsl_fft_forward);
 
-    gsl_fft_real_wavetable_free (real);
-    gsl_fft_real_workspace_free (work);
+    gsl_fft_complex_wavetable_free (complex);
+    gsl_fft_complex_workspace_free (work);
     for(int i = 0; i< length; i++)
     {
         input[i] = sqrt(reals[i*2]*reals[i*2] + reals[i*2+1]*reals[i*2+1])/length;
