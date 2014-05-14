b082df9e3aa75bb314ef79aeb41978f47173a9f6
Sergey Negrashov <sergey@openpowerquality.com>
2014-05-13 15:53:03 -1000

    Exclusion of edges for the rms calculation

--- rpi-firmware/rpid/src/analysistask.cpp
+++ rpi-firmware/rpid/src/analysistask.cpp
@@ -21,7 +21,23 @@ void AnalysisTask::run()
             OpqSetting frequency = OpqSetting((float)(SAMPLING_RATE*gausianPeak(next)/(next->fft.size())));
 
             next->parameters["f"] = frequency;
-            next->parameters["vrms"] = (float)(VOLTAGE_SCALING*rmsVoltage(next->data));
+            std::vector<double> data;
+            int start = 0;
+            int end = 0;
+            for(int i = 0; i< next->data.size() -1; i++)
+            {
+                if(start == 0)
+                {
+                    if(data[i]*data[i-1] < 0)
+                        start = i;
+                }
+                else if(data[i]*data[i-1] < 0)
+                        end = i;
+            }
+            data.assign(end - start, 0);
+            std::copy(next->data.begin() + start, next->data.begin() + end, data.begin());
+            next->parameters["vrms"] = (float)(VOLTAGE_SCALING*rmsVoltage(data));
+
             next->parameters["thd"] = "TO DO";
             oq_->push(next);
             boost::this_thread::interruption_point();
