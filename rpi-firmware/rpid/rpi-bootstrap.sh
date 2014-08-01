f900bf140e9fd1b0c37376d6135b531aa1110082
anthonyjchriste <achriste@hawaii.edu>
2014-08-01 00:50:25 +0000

    Update fstab on bootstrap

--- rpi-firmware/rpid/rpi-bootstrap.sh
+++ rpi-firmware/rpid/rpi-bootstrap.sh
@@ -4,7 +4,9 @@
 echo "Installing dependicies from apt..."
 apt-get install time libgsl0-dev libboost-dev libboost-filesystem-dev vim-nox libboost-thread-dev eject zenity
 
-# Install usb monitor
-echo "Installing usb monitoring and auto-mounting..."
-install contrib/devmon /usr/local/bin/devmon
+# Make sure usb mount point exists
+mkdir /media/usb
+
+# Setup fstab
+echo "/dev/sda1	/media/usb	vfat	rw,defaults	0	0" >> /etc/fstab
 
